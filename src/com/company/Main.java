4cd8ad80bdc20b6ef7cf5dd02ff92e21d1d50efb
songbo <874756030@qq.com>
2017-12-01 13:37:16 +0800

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        System.out.println("宋老师");//输出语句
+        System.out.println(25);
+        System.out.println("hello world");
+
+    }
+}
